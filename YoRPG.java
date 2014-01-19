0ae19830f49ca9ab593a100c8ea7fd0230f57144
mariavasilkin <m.vasilkin@gmail.com>
2014-01-19 18:16:17 -0500

    Level stuffs

--- YoRPG.java
+++ YoRPG.java
@@ -232,7 +232,9 @@ public class YoRPG {
 	else if (wins >= 3) {
 	    if (levelCount < 3) { //bc the last time shouldn't prompt you for the next level
 		try {
-		    System.out.println( "Good job, man. Now what?" );
+		    System.out.println( "Good job, man. You plaeyd " + roundCount
+		    			+ "rounds, and have " + (3- losses) + "lives remaining.
+		    			/n Now what?" );
 		    System.out.println( "\t1: Keep playing level. \n\t2: Proceed to next level" );
 		    i = Integer.parseInt( in.readLine() );
 		}
